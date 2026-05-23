0fe0cb4a92cd0c3e29168aea0162c775d65d99b0
MuzlikGamer <123552099+muzlik-gm@users.noreply.github.com>
2026-05-23 21:39:57 +0500

    Delete scratch directory

--- scratch/fix_syntax.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let src = fs.readFileSync('src/main.js', 'utf8');
-
-src = src.replace("});\n\n \n});", "");
-src = src.replace("});\r\n\r\n \r\n});", "");
-
-fs.writeFileSync('src/main.js', src, 'utf8');
